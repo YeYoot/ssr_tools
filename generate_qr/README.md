2d500b9a1386dbdbfeaf06cfcc9d7bf1ffccb92a
Yeat <yeyoutao.1995@gmail.com>
2019-08-25 09:56:24 +0800

    upload generate_qr.exe

--- generate_qr/README.md
+++ generate_qr/README.md
@@ -1,22 +1,11 @@
 # generate_qr
-批量生成ss二维码
 
-依赖库：
+说明：批量生成ss二维码
 
-- xlrd
-- MyQR
+## 使用方法
 
-## 使用说明
-
-1. 将需要生成二维码的端口，密码，协议及IP填入到config.xlsx表格中
-
-2. 运行脚本
-
-   ```
-   python generate_qr.py
-   ```
-
-注：运行环境为python3（建议3.7），建议在本地pc上运行
+1. 将ss节点的配置信息填入到input/config.xlsx表格中并保存。
+2. 双击generate_qr.exe
 
 
 
---
+++ generate_qr/src/generate_qr.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import os
+import xlrd
+import base64
+import logging
+from MyQR import myqr
+
+
+def get_config(excel_name, sheet_name):
+    logging.info("读取{}表格，{}页签。")
+    excel_name = os.path.join(os.getcwd(), "input", excel_name)
+    try:
+        wb = xlrd.open_workbook(excel_name)
+        ws = wb.sheet_by_name(sheet_name)
+    except:
+        logging.info("打开EXCEL表格失败，请确认{0}文件，{1}页签是否存在。".format(excel_name, sheet_name))
+        exit(-1)
+    col = {
+        "IP": -1,
+        "端口": -1,
+        "密码": -1,
+        "协议": -1,
+    }
+    for i in range(0, ws.ncols):
+        for key, val in col.items():
+            if ws.cell(rowx=0, colx=i).value == key:
+                col[key] = i
+    config = {}
+    for j in range(1, ws.nrows):
+        config.update({
+            str(int(ws.cell(rowx=j, colx=col["端口"]).value)):
+                {
+                    "password": str(ws.cell(rowx=j, colx=col["密码"]).value),
+                    "ip": str(ws.cell(rowx=j, colx=col["IP"]).value),
+                    "method": str(ws.cell(rowx=j, colx=col["协议"]).value),
+
+                }})
+
+    return config
+
+
+def generate_qr(config, picture="ye.png"):
+    output_dir = os.path.join(os.getcwd(), "二维码")
+    picture = os.path.join(os.getcwd(), "input", picture)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    for port, val in config.items():
+        ori_str = val["method"] + ":" + val["password"] + "@" + val["ip"] + ":" + port
+        encode_str = base64.b64encode(ori_str.encode('utf-8'))
+        des_str = "ss://".encode('utf-8') + encode_str
+        pic_name = str(port) + ".jpg"
+        myqr.run(des_str.decode(), save_name=pic_name, picture=picture, save_dir=output_dir)
+
+
+def main():
+    config = get_config("config.xlsx", "Sheet1")
+    generate_qr(config)
+
+
+if __name__ == '__main__':
+    main()
